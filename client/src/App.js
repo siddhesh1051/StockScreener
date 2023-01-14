f7c676575b33ed1ec34f14703e1a883af3eb1797
Hem <hpmahimkar@gmail.com>
2023-01-14 14:26:21 +0530

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Navbar from "../src/components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar />
     </div>
   );
 }
