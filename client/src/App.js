67348ec7a13618c3592c48b0f44914714bc52227
Hem <hpmahimkar@gmail.com>
2023-01-14 16:25:35 +0530

    no chnges

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar/Navbar";
-// import stockdets from "./pages/stockdets";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
     </div>
   );
 }
