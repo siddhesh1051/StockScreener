a7a45436da78e582cf189d2b789b05e11ffa4a74
siddhesh1051 <siddhesh1052001@gmail.com>
2023-01-14 16:42:28 +0530

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './components/About/About';
+import Index from './components/Index/Index';
+import Stocks from './components/Stocks/Stocks';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router> 
+    <Navbar/>
+    
+            <Routes>
+           
+              
+                <Route path="/" element = {<Home/>}></Route>
+                <Route path="/about" element={<About/>}></Route>
+                <Route path="/index" element={<Index/>}></Route>
+                <Route path="/stocks" element={<Stocks/>}></Route>
+                
+              
+            </Routes>
+        </Router>
     </div>
   );
 }
