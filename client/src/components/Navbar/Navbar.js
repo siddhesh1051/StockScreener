09ab99c7d43c9bcdf827ebebbc29ef1d334514ab
sahilkruz07 <105875309+sahilkruz07@users.noreply.github.com>
2023-01-14 15:41:58 +0530

    nav v2 added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,10 +4,20 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <>
-    
-    <div className="outCont">
-    
-    </div>
+      <div className="navbar-container">
+            <div className="logo-container">
+                TrueStocker
+            </div>
+            <div className="nav-menu">
+                <div className="nav-menu-item">About</div>
+                <div className="nav-menu-item">Stocks</div>
+                <div className="nav-menu-item">Index</div>
+            </div>
+            <div className="login-menu">
+                <div className="login-prof-pic"></div>
+                <div className="login-prof-name">Sahil Kanganee</div>
+            </div>
+        </div>
     
     
     </>
