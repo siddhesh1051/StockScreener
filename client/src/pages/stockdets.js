d83a0c2dad6a9266b7a5f8244131d79cb3148fad
sahilkruz07 <105875309+sahilkruz07@users.noreply.github.com>
2023-01-14 15:52:51 +0530

    stock det page intialized

---
+++ client/src/pages/stockdets.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./stockdets.css"
+
+const stockdets = () => {
+  return (
+    <>
+    <div className="outercont">
+    SAHIL LOLER
+    </div>
+    </>
+  )
+}
+
+export default stockdets
