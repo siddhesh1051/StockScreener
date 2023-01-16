6f79450cf76ec4d35f84fa9c3b3c2e9a0fffe654
siddhesh1051 <siddhesh1052001@gmail.com>
2023-01-16 22:22:15 +0530

    client url changed

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://stock-flipr.vercel.app/",
     credentials: true,
   })
 );
